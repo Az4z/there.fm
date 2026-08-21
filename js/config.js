106ba57d7243c98681317e017768b500059f30f7
V <123498563+discernings@users.noreply.github.com>
2026-08-21 09:37:51 -0400

    Add configuration file for API keys and settings

---
+++ js/config.js
@@ -0,0 +1,7 @@
+Enterexport const CONFIG = {
+  supabaseUrl: 'https://vhflsjrawbvmokyzegef.supabase.co',
+  supabaseAnonKey: '',
+  youtubeApiKey: 'COLE_AQUI_SUA_YOUTUBE_API_KEY',
+  klipyKey: 'COLE_AQUI_SUA_KLIPY_KEY',
+  roomPrefix: 'templo-fm:room:'
+};
