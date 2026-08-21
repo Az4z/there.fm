52803a676efe7f6e7a763f57d296400c1de1613a
V <123498563+discernings@users.noreply.github.com>
2026-08-21 10:07:21 -0400

    Change roomPrefix from 'templo-fm' to 'there-fm'

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ Enterexport const CONFIG = {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZmxzanJhd2J2bW9reXplZ2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjY4OTksImV4cCI6MjA5NDU0Mjg5OX0._ej361ZjDBcnFuBHn3YIhvGmpRhZLWhG8Rth4p6ugZ0',
   youtubeApiKey: 'AIzaSyD6KXzYmg42GL9thLhmEOvKPoyjc90MMA8',
   klipyKey: 'hc7D7nAjR0hmqWoHyNrgifqGKw2TdZAIMugS0SJCOqlVk7OrIgIe1pjExXBlaz4r',
-  roomPrefix: 'templo-fm:room:'
+  roomPrefix: 'there-fm:room:'
 };
