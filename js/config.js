fdd664dced24df0b18052d60a26324cdf694d7a3
V <123498563+discernings@users.noreply.github.com>
2026-08-21 09:42:29 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 Enterexport const CONFIG = {
   supabaseUrl: 'https://vhflsjrawbvmokyzegef.supabase.co',
-  supabaseAnonKey: '',
-  youtubeApiKey: 'COLE_AQUI_SUA_YOUTUBE_API_KEY',
-  klipyKey: 'COLE_AQUI_SUA_KLIPY_KEY',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZmxzanJhd2J2bW9reXplZ2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjY4OTksImV4cCI6MjA5NDU0Mjg5OX0._ej361ZjDBcnFuBHn3YIhvGmpRhZLWhG8Rth4p6ugZ0',
+  youtubeApiKey: 'AIzaSyD6KXzYmg42GL9thLhmEOvKPoyjc90MMA8',
+  klipyKey: 'hc7D7nAjR0hmqWoHyNrgifqGKw2TdZAIMugS0SJCOqlVk7OrIgIe1pjExXBlaz4r',
   roomPrefix: 'templo-fm:room:'
 };
